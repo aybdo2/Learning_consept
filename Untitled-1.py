ca85536e2b6e6a06ef21d864812933fc2d40ba2e
Ayoub Douane <douaneayoubwork@gmail.com>
2024-04-25 10:18:30 +0100

    ss

---
+++ Untitled-1.py
@@ -0,0 +1,27 @@
+#Part 1 of the video
+#name = 22
+#name = name + 1
+#print('My names is '+str(name))
+#typesfloat0.2,string"abd23",integer234,bool
+#bool,string,int
+
+#parte 2
+'''
+name,dude,bro= 22,"sss",True
+ay=do=hi=10
+print(type(name))
+print(type(dude))
+print(type(bro))
+print(ay)
+'''
+
+#part 3
+#name='my names is jojo'
+#print(name.find ("o"))
+#print(name.capitalize())
+#print(name.upper())
+#print(name.lower())
+#print(name.isdigit())
+#print(name.count ("o"))
+#print(name.replace( "o" , "a"))
+#print(name*3)
